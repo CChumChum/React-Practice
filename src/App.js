7ed84395ce5a855ce6274732bffdf2ca64ac89ec
An Hoang Chu <cchum900@gmail.com>
2026-01-18 15:43:18 +0100

    Added components and nested it into App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
 
-  const title = "Welcome to the blog";
-  const likes = 50;
-  // const person = {name: "yoshi", age: 30};
-  const link = "https://www.youtube.com"
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked { likes } times</p>
-
-        {/* <p>{person}</p> */}
-
-        <p>{ Math.random() * 10 }</p>
-        <p>{"The number over are random"}</p>
-        <p>{"This is a list of numbers:" + [1, 2, 3, 4, 5]}</p>
-        <p>The numbers over are hard coded</p>
-
-        <a href={ link }>Youtube Site</a>
+        <Home />
       </div>
     </div>
   );
