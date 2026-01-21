59d79e0ef7ca08451bb950047a1b184f77c42ae5
An Hoang Chu <cchum900@gmail.com>
2026-01-21 01:18:10 +0100

    Made the title page clickable and made author input field

--- src/Create.js
+++ src/Create.js
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 const Create = () => {
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
-    const [author, setAuthor] = useState("mario");
+    const [author, setAuthor] = useState("");
     const [isPending, setIsPending] = useState(false);
     const history = useHistory();
 
@@ -43,13 +43,21 @@ const Create = () => {
                     onChange={(e) => setBody(e.target.value)}
                 ></textarea>
                 <label>Blog Author</label>
-                <select
+                <input 
+                    type="text"
+                    required
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                />         
+
+                {/* <select
                 value = {author}
                 onChange={(e) => setAuthor(e.target.value)}
                 >
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
-                </select>
+                </select> */}
+
                 { !isPending && <button>Add Blog</button> }
                 { isPending && <button disabled>Adding Blog...</button> }
             </form>
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,17 +3,25 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <h1>The Dojo Blog</h1>
+      <Link to="/" className="logo">
+        <h1>Simple Blog</h1>
+      </Link>
+
       <div className="links">
         <Link to="/">Home</Link>
-        <Link to="/create" style={{ 
-          color: 'white', 
-          backgroundColor: '#f1356d',
-          borderRadius: '8px' 
-        }}>New Blog</Link>
+        <Link
+          to="/create"
+          style={{
+            color: "white",
+            backgroundColor: "#f1356d",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </Link>
       </div>
     </nav>
   );
-}
- 
+};
+
 export default Navbar;
